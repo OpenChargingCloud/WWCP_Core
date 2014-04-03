103af6122ce5b69ace03ce0053fbb2241bcac407
Achim Friedland <github@ahzf.de>
2014-04-03 21:16:34 +0200

    started to add mobile OICP auth methods

--- eMI3_Core/LocalService/LocalEMobilityService.cs
+++ eMI3_Core/LocalService/LocalEMobilityService.cs
@@ -18,10 +18,8 @@
 #region Usings
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 #endregion
 
