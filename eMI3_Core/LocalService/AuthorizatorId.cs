508ad71d08ceb80222fe2736377d380612cb99ee
Achim Friedland <github@ahzf.de>
2014-04-04 00:33:05 +0200

    More on MobileRemoteStart/Stop

---
+++ eMI3_Core/LocalService/AuthorizatorId.cs
@@ -0,0 +1,395 @@
+﻿/*
+ * Copyright (c) 2013-2014 Achim Friedland <achim.friedland@belectric.com>
+ * This file is part of eMI3 Core <http://www.github.com/eMI3/Core>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace org.emi3group
+{
+
+    /// <summary>
+    /// The unique identification of an authorizator.
+    /// </summary>
+    public class AuthorizatorId : IId, IEquatable<AuthorizatorId>, IComparable<AuthorizatorId>
+    {
+
+        #region Data
+
+        /// <summary>
+        /// The internal identification.
+        /// </summary>
+        protected readonly String _Id;
+
+        #endregion
+
+        #region Properties
+
+        #region Length
+
+        /// <summary>
+        /// Returns the length of the identificator.
+        /// </summary>
+        public UInt64 Length
+        {
+            get
+            {
+                return (UInt64) _Id.Length;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region AuthorizatorId()
+
+        /// <summary>
+        /// Generate a new authorizator identification.
+        /// </summary>
+        public AuthorizatorId()
+        {
+            _Id = Guid.NewGuid().ToString();
+        }
+
+        #endregion
+
+        #region AuthorizatorId(String)
+
+        /// <summary>
+        /// Generate a new authorizator identification.
+        /// based on the given string.
+        /// </summary>
+        public AuthorizatorId(String String)
+        {
+            _Id = String.Trim();
+        }
+
+        #endregion
+
+        #endregion
+
+
+        #region New
+
+        /// <summary>
+        /// Generate a new authorizator identification.
+        /// </summary>
+        public static AuthorizatorId New
+        {
+            get
+            {
+                return new AuthorizatorId(Guid.NewGuid().ToString());
+            }
+        }
+
+        #endregion
+
+        #region Parse(EVSEOperatorId)
+
+        /// <summary>
+        /// Parse the given string as an authorizator identification.
+        /// </summary>
+        public static AuthorizatorId Parse(String EVSEOperatorId)
+        {
+            return new AuthorizatorId(EVSEOperatorId);
+        }
+
+        #endregion
+
+        #region TryParse(Text, out EVSEOperatorId)
+
+        /// <summary>
+        /// Parse the given string as an authorizator identification.
+        /// </summary>
+        public static Boolean TryParse(String Text, out AuthorizatorId EVSEOperatorId)
+        {
+            try
+            {
+                EVSEOperatorId = new AuthorizatorId(Text);
+                return true;
+            }
+            catch (Exception e)
+            {
+                EVSEOperatorId = null;
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Clone
+
+        /// <summary>
+        /// Clone an authorizator identification.
+        /// </summary>
+        public AuthorizatorId Clone
+        {
+            get
+            {
+                return new AuthorizatorId(_Id);
+            }
+        }
+
+        #endregion
+
+
+        #region Operator overloading
+
+        #region Operator == (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator == (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+
+            // If both are null, or both are same instance, return true.
+            if (Object.ReferenceEquals(AuthorizatorId1, AuthorizatorId2))
+                return true;
+
+            // If one is null, but not both, return false.
+            if (((Object) AuthorizatorId1 == null) || ((Object) AuthorizatorId2 == null))
+                return false;
+
+            return AuthorizatorId1.Equals(AuthorizatorId2);
+
+        }
+
+        #endregion
+
+        #region Operator != (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator != (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+            return !(AuthorizatorId1 == AuthorizatorId2);
+        }
+
+        #endregion
+
+        #region Operator <  (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator < (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+
+            if ((Object) AuthorizatorId1 == null)
+                throw new ArgumentNullException("The given AuthorizatorId1 must not be null!");
+
+            return AuthorizatorId1.CompareTo(AuthorizatorId2) < 0;
+
+        }
+
+        #endregion
+
+        #region Operator <= (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator <= (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+            return !(AuthorizatorId1 > AuthorizatorId2);
+        }
+
+        #endregion
+
+        #region Operator >  (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator > (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+
+            if ((Object) AuthorizatorId1 == null)
+                throw new ArgumentNullException("The given AuthorizatorId1 must not be null!");
+
+            return AuthorizatorId1.CompareTo(AuthorizatorId2) > 0;
+
+        }
+
+        #endregion
+
+        #region Operator >= (AuthorizatorId1, AuthorizatorId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId1">A AuthorizatorId.</param>
+        /// <param name="AuthorizatorId2">Another AuthorizatorId.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator >= (AuthorizatorId AuthorizatorId1, AuthorizatorId AuthorizatorId2)
+        {
+            return !(AuthorizatorId1 < AuthorizatorId2);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IComparable<AuthorizatorId> Members
+
+        #region CompareTo(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        public Int32 CompareTo(Object Object)
+        {
+
+            if (Object == null)
+                throw new ArgumentNullException("The given object must not be null!");
+
+            // Check if the given object is an AuthorizatorId.
+            var AuthorizatorId = Object as AuthorizatorId;
+            if ((Object) AuthorizatorId == null)
+                throw new ArgumentException("The given object is not a AuthorizatorId!");
+
+            return CompareTo(AuthorizatorId);
+
+        }
+
+        #endregion
+
+        #region CompareTo(AuthorizatorId)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="AuthorizatorId">An object to compare with.</param>
+        public Int32 CompareTo(AuthorizatorId AuthorizatorId)
+        {
+
+            if ((Object) AuthorizatorId == null)
+                throw new ArgumentNullException("The given AuthorizatorId must not be null!");
+
+            // Compare the length of the AuthorizatorIds
+            var _Result = this.Length.CompareTo(AuthorizatorId.Length);
+
+            // If equal: Compare Ids
+            if (_Result == 0)
+                _Result = _Id.CompareTo(AuthorizatorId._Id);
+
+            return _Result;
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IEquatable<AuthorizatorId> Members
+
+        #region Equals(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        /// <returns>true|false</returns>
+        public override Boolean Equals(Object Object)
+        {
+
+            if (Object == null)
+                return false;
+
+            // Check if the given object is an AuthorizatorId.
+            var AuthorizatorId = Object as AuthorizatorId;
+            if ((Object) AuthorizatorId == null)
+                return false;
+
+            return this.Equals(AuthorizatorId);
+
+        }
+
+        #endregion
+
+        #region Equals(AuthorizatorId)
+
+        /// <summary>
+        /// Compares two AuthorizatorIds for equality.
+        /// </summary>
+        /// <param name="AuthorizatorId">A AuthorizatorId to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(AuthorizatorId AuthorizatorId)
+        {
+
+            if ((Object) AuthorizatorId == null)
+                return false;
+
+            return _Id.Equals(AuthorizatorId._Id);
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region GetHashCode()
+
+        /// <summary>
+        /// Return the HashCode of this object.
+        /// </summary>
+        /// <returns>The HashCode of this object.</returns>
+        public override Int32 GetHashCode()
+        {
+            return _Id.GetHashCode();
+        }
+
+        #endregion
+
+        #region ToString()
+
+        /// <summary>
+        /// Return a string represtentation of this object.
+        /// </summary>
+        public override String ToString()
+        {
+            return _Id.ToString();
+        }
+
+        #endregion
+
+    }
+
+}
