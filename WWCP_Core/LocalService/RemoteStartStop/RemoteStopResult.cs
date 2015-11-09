1c3e0c8710b099b368cedc02dba7a7796e6a2f68
Achim Friedland <github@ahzf.de>
2015-11-09 05:25:54 +0100

    Timing issues

--- WWCP_Core/LocalService/RemoteStartStop/RemoteStopResult.cs
+++ WWCP_Core/LocalService/RemoteStartStop/RemoteStopResult.cs
@@ -24,6 +24,7 @@ namespace org.GraphDefined.WWCP.LocalService
         Stop_Timeout,
         UnknownEVSE,
         EVSEOutOfService,
+        SessionIsInvalid,
         Error,
         Success
     }
