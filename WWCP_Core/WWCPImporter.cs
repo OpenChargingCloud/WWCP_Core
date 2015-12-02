0ef173daf5877a2c33ac8675f073d9a6197ca119
Achim Friedland <github@ahzf.de>
2015-12-02 06:01:52 +0100

    Some refactorings

--- WWCP_Core/WWCPImporter.cs
+++ WWCP_Core/WWCPImporter.cs
@@ -30,7 +30,7 @@ using Newtonsoft.Json.Linq;
 
 using org.GraphDefined.Vanaheimr.Illias;
 using org.GraphDefined.Vanaheimr.Hermod.HTTP;
-using org.GraphDefined.Vanaheimr.Hermod.Services.DNS;
+using org.GraphDefined.Vanaheimr.Hermod.DNS;
 
 #endregion
 
