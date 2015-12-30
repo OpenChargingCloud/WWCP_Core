d0efca9f26bf41a0663b54e4acf888937733ec64
Achim Friedland <github@ahzf.de>
2015-12-30 01:00:51 +0100

    Updates

--- WWCP_Core_UnitTests/Properties/AssemblyInfo.cs
+++ WWCP_Core_UnitTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GraphDefined GmbH")]
 [assembly: AssemblyProduct("WWCP Core UnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 GraphDefined GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 GraphDefined GmbH <achim.friedland@graphdefined.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
