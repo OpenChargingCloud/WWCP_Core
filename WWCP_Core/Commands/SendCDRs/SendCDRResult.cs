a6b0db1e1726fd1dd3d8c2ff706ee2ac6c972136
Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
2020-07-05 03:05:35 +0200

    Refactorings

--- WWCP_Core/Commands/SendCDRs/SendCDRResult.cs
+++ WWCP_Core/Commands/SendCDRs/SendCDRResult.cs
@@ -1016,10 +1016,10 @@ namespace org.GraphDefined.WWCP
                 if (VerifyContext)
                 {
 
-                    if (!JSONObject.ParseMandatory("@context",
-                                                   "JSON-LD context",
-                                                   out String Context,
-                                                   out ErrorResponse))
+                    if (!JSONObject.ParseMandatoryText("@context",
+                                                       "JSON-LD context",
+                                                       out String Context,
+                                                       out ErrorResponse))
                     {
                         ErrorResponse = @"The JSON-LD ""@context"" information is missing!";
                         return false;
