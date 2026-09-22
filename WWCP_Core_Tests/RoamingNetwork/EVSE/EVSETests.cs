OpenChargingCloud/WWCP_Core

commit 7530c68a827e0d23a0c37abe98489663af569d2d

Some refactorings

WWCP_Core_Tests/RoamingNetwork/EVSE/EVSETests.cs
@@ -641,17 +641,15 @@ namespace cloud.charging.open.protocols.WWCP.tests.RoamingNetwork
                                                 TariffURL:       null,
                                                 Currency:        Currency.EUR,
                                                 EnergyMix:       null,
-                                                TariffElements:  new ChargingTariffElement[] {
+                                                TariffElements:  [
                                                                      new ChargingTariffElement(
-
                                                                          new ChargingPriceComponent(
                                                                              Type:       ChargingDimensionTypes.ENERGY,
                                                                              Price:      0.25M,
                                                                              StepSize:   1000
                                                                          )
-
                                                                      )
-                                                                 }
+                                                                 ]
                                             );
                     ClassicAssert.IsNotNull(tariff_0_25_kWh);
 
@@ -660,10 +658,10 @@ namespace cloud.charging.open.protocols.WWCP.tests.RoamingNetwork
                         tariff_0_25_kWh   is not null)
                     {
 
-                        DE_GEF_E0001_AAAA_1.ChargingTariffs.Add(tariff_3_98_60min);
-                        DE_GEF_E0001_AAAA_1.ChargingTariffs.Add(tariff_0_25_kWh);
+                        //DE_GEF_E0001_AAAA_1.ChargingTariffs.Add(tariff_3_98_60min);
+                        //DE_GEF_E0001_AAAA_1.ChargingTariffs.Add(tariff_0_25_kWh);
 
-                        ClassicAssert.AreEqual(2, DE_GEF_E0001_AAAA_1.ChargingTariffs.   Count);
+                        //ClassicAssert.AreEqual(2, DE_GEF_E0001_AAAA_1.ChargingTariffs.   Count);
 
                         ClassicAssert.AreEqual(2, evseDataChanges.                       Count);
                         ClassicAssert.AreEqual(2, chargingStationEVSEDataChanges.        Count);
@@ -676,9 +674,7 @@ namespace cloud.charging.open.protocols.WWCP.tests.RoamingNetwork
                                                       Id:         EVSEGroup_Id.Parse(DE_GEF.Id, tariffGroup_VW.Id, "_T0.25_per_kWh"),
                                                       Name:       I18NString.Create(Languages.de, "0,25 € / kWh"),
                                                       Tariff:     tariff_0_25_kWh,
-                                                      MemberIds:  new[] {
-                                                                      DE_GEF_E0001_AAAA_1.Id
-                                                                  }
+                                                      MemberIds:  [ DE_GEF_E0001_AAAA_1.Id ]
                                                   );
 
                         ClassicAssert.IsNotNull(evseGroup_0_25_kWh);

WWCP_Core/Entities/ChargingPool/ChargingPool_Id.cs
@@ -222,7 +222,7 @@ namespace cloud.charging.open.protocols.WWCP
         public static ChargingPool_Id Parse(String Text)
         {
 
-            if (TryParse(Text, out ChargingPool_Id chargingPoolId))
+            if (TryParse(Text, out var chargingPoolId))
                 return chargingPoolId;
 
             throw new ArgumentException($"Invalid text representation of a charging pool identification: '{Text}'!",

WWCP_Core/Entities/EVSE/ChargingConnector/ChargingConnector.cs
@@ -68,44 +68,49 @@ namespace cloud.charging.open.protocols.WWCP
         /// The parent EVSE of this charging connector.
         /// </summary>
         [InternalUseOnly]
-        public IEVSE?                EVSE             { get; set; }
+        public IEVSE?                          EVSE             { get; set; }
 
         /// <summary>
         /// The optional charging connector identification.
         /// </summary>
         [Mandatory]
-        public ChargingConnector_Id  Id               { get; }
+        public ChargingConnector_Id            Id               { get; }
 
         /// <summary>
         /// The type of the charging plug.
         /// </summary>
         [Mandatory]
-        public ChargingPlugTypes     Plug             { get; }
+        public ChargingPlugTypes               Plug             { get; }
 
         /// <summary>
         /// Whether the charging plug is lockable or not.
         /// </summary>
         [Optional]
-        public Boolean?              Lockable         { get; }
+        public Boolean?                        Lockable         { get; }
 
         /// <summary>
         /// Whether the charging plug has an attached cable or not.
         /// </summary>
         [Optional]
-        public Boolean               CableAttached    { get; }
+        public Boolean                         CableAttached    { get; }
 
         /// <summary>
         /// The length of the charging cable.
         /// </summary>
         [Optional]
-        public Meter?                CableLength      { get; }
+        public Meter?                          CableLength      { get; }
 
         /// <summary>
         /// Whether the charging connector is DC or AC.
         /// </summary>
         [Mandatory]
-        public Boolean               IsDC
+        public Boolean                         IsDC
             => Plug.IsDC();
+
+        /// <summary>
+        /// The tariffs that can be used with this charging connector.
+        /// </summary>
+        public IEnumerable<ChargingTariff_Id>  Tariffs          { get; }
 
         #endregion
 
@@ -119,11 +124,12 @@ namespace cloud.charging.open.protocols.WWCP
         /// <param name="Lockable">Whether the charging plug is lockable or not.</param>
         /// <param name="CableAttached">The type of the charging cable.</param>
         /// <param name="CableLength">The length of the charging cable.</param>
-        public ChargingConnector(ChargingConnector_Id  Id,
-                                 ChargingPlugTypes     Plug,
-                                 Boolean?              Lockable        = null,
-                                 Boolean               CableAttached   = false,
-                                 Meter?                CableLength     = null)
+        public ChargingConnector(ChargingConnector_Id             Id,
+                                 ChargingPlugTypes                Plug,
+                                 Boolean?                         Lockable        = null,
+                                 Boolean                          CableAttached   = false,
+                                 Meter?                           CableLength     = null,
+                                 IEnumerable<ChargingTariff_Id>?  Tariffs         = null)
         {
 
             this.Id             = Id;
@@ -131,6 +137,7 @@ namespace cloud.charging.open.protocols.WWCP
             this.Lockable       = Lockable;
             this.CableAttached  = CableAttached;
             this.CableLength    = CableLength;
+            this.Tariffs        = Tariffs ?? [];
 
             unchecked
             {
@@ -154,12 +161,13 @@ namespace cloud.charging.open.protocols.WWCP
         /// <param name="Lockable">Whether the charging plug is lockable or not.</param>
         /// <param name="CableAttached">The type of the charging cable.</param>
         /// <param name="CableLength">The length of the charging cable.</param>
-        public ChargingConnector(IEVSE?                ParentEVSE,
-                                 ChargingConnector_Id  Id,
-                                 ChargingPlugTypes     Plug,
-                                 Boolean?              Lockable        = null,
-                                 Boolean               CableAttached   = false,
-                                 Meter?                CableLength     = null)
+        public ChargingConnector(IEVSE?                           ParentEVSE,
+                                 ChargingConnector_Id             Id,
+                                 ChargingPlugTypes                Plug,
+                                 Boolean?                         Lockable        = null,
+                                 Boolean                          CableAttached   = false,
+                                 Meter?                           CableLength     = null,
+                                 IEnumerable<ChargingTariff_Id>?  Tariffs         = null)
         {
 
             this.EVSE           = ParentEVSE;
@@ -168,6 +176,7 @@ namespace cloud.charging.open.protocols.WWCP
             this.Lockable       = Lockable;
             this.CableAttached  = CableAttached;
             this.CableLength    = CableLength;
+            this.Tariffs        = Tariffs ?? [];
 
             unchecked
             {
@@ -190,10 +199,11 @@ namespace cloud.charging.open.protocols.WWCP
         /// <param name="Lockable">Whether the charging plug is lockable or not.</param>
         /// <param name="CableAttached">The type of the charging cable.</param>
         /// <param name="CableLength">The length of the charging cable.</param>
-        public ChargingConnector(ChargingPlugTypes  Plug,
-                                 Boolean?           Lockable        = null,
-                                 Boolean            CableAttached   = false,
-                                 Meter?             CableLength     = null)
+        public ChargingConnector(ChargingPlugTypes                Plug,
+                                 Boolean?                         Lockable        = null,
+                                 Boolean                          CableAttached   = false,
+                                 Meter?                           CableLength     = null,
+                                 IEnumerable<ChargingTariff_Id>?  Tariffs         = null)
         {
 
             this.Id             = ChargingConnector_Id.Parse(1);
@@ -201,6 +211,7 @@ namespace cloud.charging.open.protocols.WWCP
             this.Lockable       = Lockable;
             this.CableAttached  = CableAttached;
             this.CableLength    = CableLength;
+            this.Tariffs        = Tariffs ?? [];
 
             unchecked
             {
@@ -223,11 +234,12 @@ namespace cloud.charging.open.protocols.WWCP
         /// <param name="Lockable">Whether the charging plug is lockable or not.</param>
         /// <param name="CableAttached">The type of the charging cable.</param>
         /// <param name="CableLength">The length of the charging cable.</param>
-        public ChargingConnector(IEVSE?             ParentEVSE,
-                                 ChargingPlugTypes  Plug,
-                                 Boolean?           Lockable        = null,
-                                 Boolean            CableAttached   = false,
-                                 Meter?             CableLength     = null)
+        public ChargingConnector(IEVSE?                           ParentEVSE,
+                                 ChargingPlugTypes                Plug,
+                                 Boolean?                         Lockable        = null,
+                                 Boolean                          CableAttached   = false,
+                                 Meter?                           CableLength     = null,
+                                 IEnumerable<ChargingTariff_Id>?  Tariffs         = null)
         {
 
             this.EVSE           = ParentEVSE;
@@ -236,6 +248,7 @@ namespace cloud.charging.open.protocols.WWCP
             this.Lockable       = Lockable;
             this.CableAttached  = CableAttached;
             this.CableLength    = CableLength;
+            this.Tariffs        = Tariffs ?? [];
 
             unchecked
             {

WWCP_Core/Entities/EVSE/ChargingConnector/IChargingConnector.cs
@@ -33,14 +33,16 @@ namespace cloud.charging.open.protocols.WWCP
         /// The EVSE of this charging connector.
         /// </summary>
         [InternalUseOnly]
-        IEVSE?                EVSE             { get; set; }
+        IEVSE?                          EVSE             { get; set; }
 
-        ChargingConnector_Id  Id               { get; }
-        Boolean               CableAttached    { get; }
-        Meter?                CableLength      { get; }
-        Boolean?              Lockable         { get; }
-        ChargingPlugTypes     Plug             { get; }
-        Boolean               IsDC             { get; }
+        ChargingConnector_Id            Id               { get; }
+        Boolean                         CableAttached    { get; }
+        Meter?                          CableLength      { get; }
+        Boolean?                        Lockable         { get; }
+        ChargingPlugTypes               Plug             { get; }
+        Boolean                         IsDC             { get; }
+
+        IEnumerable<ChargingTariff_Id>  Tariffs          { get; }
 
         JObject? ToJSON(Boolean                                              Embedded                            = false,
                         CustomJObjectSerializerDelegate<ChargingConnector>?  CustomChargingConnectorSerializer   = null);

WWCP_Core/Entities/EVSE/EVSE.cs
@@ -139,14 +139,14 @@ namespace cloud.charging.open.protocols.WWCP
         [Mandatory, SlowData]
         public ReactiveSet<ChargingModes>               ChargingModes               { get; }
 
-        /// <summary>
-        /// An enumeration of all available charging tariffs at this EVSE.
-        /// </summary>
-        [Optional, SlowData]
-        public ReactiveSet<ChargingTariff>              ChargingTariffs             { get; }
+        ///// <summary>
+        ///// An enumeration of all available charging tariffs at this EVSE.
+        ///// </summary>
+        //[Optional, SlowData]
+        //public ReactiveSet<ChargingTariff>              ChargingTariffs             { get; }
 
 
-        public Dictionary<EMPRoamingProvider_Id, List<ChargingTariff_Id>>  ChargingTariffIds    { get; }
+        //public Dictionary<EMPRoamingProvider_Id, List<ChargingTariff_Id>>  ChargingTariffIds    { get; }
 
 
         #region CurrentType
@@ -963,17 +963,17 @@ namespace cloud.charging.open.protocols.WWCP
 
             };
 
-            this.ChargingTariffs                    = ChargingTariffs is null
-                                                          ? new ReactiveSet<ChargingTariff>()
-                                                          : new ReactiveSet<ChargingTariff>(ChargingTariffs);
-            this.ChargingTariffs.OnSetChanged      += (timestamp, reactiveSet, newItems, oldItems) =>
-            {
+            //this.ChargingTariffs                    = ChargingTariffs is null
+            //                                              ? new ReactiveSet<ChargingTariff>()
+            //                                              : new ReactiveSet<ChargingTariff>(ChargingTariffs);
+            //this.ChargingTariffs.OnSetChanged      += (timestamp, reactiveSet, newItems, oldItems) =>
+            //{
 
-                PropertyChanged("ChargingTariffs",
-                                oldItems,
-                                newItems);
+            //    PropertyChanged("ChargingTariffs",
+            //                    oldItems,
+            //                    newItems);
 
-            };
+            //};
 
             this.currentType                        = CurrentType ?? CurrentTypes.AC_ThreePhases;
 

WWCP_Core/Entities/EVSE/Interfaces/IEVSE.cs
@@ -173,11 +173,11 @@ namespace cloud.charging.open.protocols.WWCP
         [Mandatory, SlowData]
         ReactiveSet<ChargingModes>             ChargingModes                { get; }
 
-        /// <summary>
-        /// An enumeration of all available charging tariffs at this EVSE.
-        /// </summary>
-        [Optional, SlowData]
-        ReactiveSet<ChargingTariff>            ChargingTariffs              { get; }
+        ///// <summary>
+        ///// An enumeration of all available charging tariffs at this EVSE.
+        ///// </summary>
+        //[Optional, SlowData]
+        //ReactiveSet<ChargingTariff>            ChargingTariffs              { get; }
 
 
         /// <summary>

commit 0e7a731dd0707fc29ba3228a9567270a9c1693ff

Retire five assertions about a feature that was switched off
EVSE_Tariff_Test has been failing since 7530c68a in December 2024, on

  Expected: 2
  But was:  0

and the reason is three lines above the failure. That commit commented out
the two DE_GEF_E0001_AAAA_1.ChargingTariffs.Add calls and the assertion on
ChargingTariffs.Count, but left the five assertions that count the property
changes those Add calls raised. The test has been checking the effects of
code somebody disabled.

It is not the test that is wrong. EVSE.ChargingTariffs is commented out in
the model as well - the property, the ReactiveSet, and the OnSetChanged
handler that fired the events - so nothing can raise them, the lists stay
empty, and no arrangement of this test can make them hold two.

So the five go where the other three already are: commented, in place, next
to the calls they belong to, with a note saying what would bring them back.
Deleting them would lose the only written record of what an EVSE used to do
with tariffs.

The suite is 293 of 293 now, for the first time in nine months.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

WWCP_Core_Tests/RoamingNetwork/EVSE/EVSETests.cs
@@ -663,11 +663,22 @@ namespace cloud.charging.open.protocols.WWCP.tests.RoamingNetwork
 
                         //ClassicAssert.AreEqual(2, DE_GEF_E0001_AAAA_1.ChargingTariffs.   Count);
 
-                        ClassicAssert.AreEqual(2, evseDataChanges.                       Count);
-                        ClassicAssert.AreEqual(2, chargingStationEVSEDataChanges.        Count);
-                        ClassicAssert.AreEqual(2, chargingPoolEVSEDataChanges.           Count);
-                        ClassicAssert.AreEqual(2, chargingStationOperatorEVSEDataChanges.Count);
-                        ClassicAssert.AreEqual(2, roamingNetworkEVSEDataChanges.         Count);
+                        // And these five belong with them. They count the property
+                        // changes those two Add calls raised, and EVSE.ChargingTariffs
+                        // is commented out in the model too - the ReactiveSet and the
+                        // OnSetChanged handler that fired the events. So nothing can
+                        // raise them: the lists are empty, and this test has been
+                        // failing on "expected 2, but was 0" ever since 7530c68a in
+                        // December 2024.
+                        //
+                        // Left commented rather than deleted, next to the two calls
+                        // they belong to, so that putting tariffs back on an EVSE puts
+                        // these back with them.
+                        //ClassicAssert.AreEqual(2, evseDataChanges.                       Count);
+                        //ClassicAssert.AreEqual(2, chargingStationEVSEDataChanges.        Count);
+                        //ClassicAssert.AreEqual(2, chargingPoolEVSEDataChanges.           Count);
+                        //ClassicAssert.AreEqual(2, chargingStationOperatorEVSEDataChanges.Count);
+                        //ClassicAssert.AreEqual(2, roamingNetworkEVSEDataChanges.         Count);
 
 
                         var evseGroup_0_25_kWh  = DE_GEF.CreateEVSEGroup(